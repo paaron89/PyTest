9e56b201242a2538a0ef08132a35c94537c97610
paaron89 <presztaron89@gmail.com>
2019-01-23 16:06:44 +0100

    First test script

--- BaseTest.py
+++ BaseTest.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
-class BaseTest:
-    def get_driver(self):
-        driver = webdriver.Chrome()
-        driver.get('http://store.demoqa.com/')
+def get_driver():
+    driver = webdriver.Chrome('/home/aron/Automation/drivers/chromedriver')
+    driver.get('http://store.demoqa.com/')
+    search = driver.find_element(By.NAME, 's')
+
+    search.clear()
+    search.send_keys('iphone')
+    search.send_keys(Keys.ENTER)
 
 
-    get_driver()
+
+
+
+get_driver()
