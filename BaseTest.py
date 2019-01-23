d2d9ed15cd678554ef8b2169e221a749216cb2ff
paaron89 <presztaron89@gmail.com>
2019-01-23 16:55:01 +0100

    added unit test and changed test site to windguru.cz

--- BaseTest.py
+++ BaseTest.py
@@ -1,19 +1,21 @@
+from unittest import TestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-def get_driver():
-    driver = webdriver.Chrome('/home/aron/Automation/drivers/chromedriver')
-    driver.get('http://store.demoqa.com/')
-    search = driver.find_element(By.NAME, 's')
+class BaseTest(TestCase):
 
-    search.clear()
-    search.send_keys('iphone')
-    search.send_keys(Keys.ENTER)
+    def test_get_driver(self):
+        driver = webdriver.Chrome('/home/aron/Automation/drivers/chromedriver')
+        driver.get('https://www.windguru.cz')
+        search = driver.find_element(By.ID,'searchspot')
+
+        search.clear()
+        search.send_keys('iphone')
+        search.send_keys(Keys.ENTER)
 
 
 
 
-
-get_driver()
