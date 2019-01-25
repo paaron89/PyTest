3f84fa1f297f7b1d006589a07d184d0e2ee43c1c
paaron89 <presztaron89@gmail.com>
2019-01-25 10:24:11 +0100

    alertBox added to locators

--- pageObjects/loginPage.py
+++ pageObjects/loginPage.py
@@ -9,16 +9,17 @@ class LoginPage:
         self.usernameBox_ID = 'email'
         self.passwordBox_ID = 'passwd'
         self.loginButton_ID = 'SubmitLogin'
+        self.alertBox_Xpath = '//*[@id="center_column"]/div[1]/p'
 
     def login(self,username, password):
         usernameBox = self.driver.find_element(By.ID, self.usernameBox_ID)
         passwordBox = self.driver.find_element(By.ID, self.passwordBox_ID)
         loginButtonBox = self.driver.find_element(By.ID, self.loginButton_ID)
+        alertBox = self.driver.find_element(By.XPATH, self.alertBox_Xpath)
 
         usernameBox.clear()
         usernameBox.send_keys(username)
-
         passwordBox.clear()
         passwordBox.send_keys(password)
-
         loginButtonBox.click()
+
